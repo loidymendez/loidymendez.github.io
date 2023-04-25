850443eab8982f32b676f54717dc7016c4f51f99
loidymendez <118315543+loidymendez@users.noreply.github.com>
2023-04-25 16:45:10 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("toggle").onclick = () =>{
+    document.getElementById("blockItems").classList.toggle("open");
+    document.getElementById("toggle").classList.toggle("close");
+}
+
