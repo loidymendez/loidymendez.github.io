73d022646bf72e30b0f0c62476f033416a022f57
loidymendez <118315543+loidymendez@users.noreply.github.com>
2023-04-25 16:55:27 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("toggle").onclick = () =>{
-    document.getElementById("blockItems").classList.toggle("open");
-    document.getElementById("toggle").classList.toggle("close");
-}
-
